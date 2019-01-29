e9751cb66a510ecc7a2444c4e7fab9b5130e280e
Julien Veyssier <eneiluj@posteo.net>
2019-01-29 03:35:56 +0100

    beginning to play with design stuff

--- templates/main.php
+++ templates/main.php
@@ -10,6 +10,9 @@ style('spend', 'pspend');
 ?>
 
 <div id="app">
+    <div id="app-navigation">
+            <?php print_unescaped($this->inc('mainnavigation')); ?>
+    </div>
     <div id="app-content">
             <?php print_unescaped($this->inc('maincontent')); ?>
     </div>
---
+++ templates/mainnavigation.php
@@ -0,0 +1,24 @@
+<?php
+// todo ;-)
+?>
+<ul>
+        <li><a class="icon-user" href="#">
+                <span>First level entry</span>
+            </a>
+            <div class="app-navigation-entry-utils">
+                <ul>
+                    <li class="app-navigation-entry-utils-counter">1</li>
+                    <li class="app-navigation-entry-utils-menu-button">
+                        <button></button>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li>
+            <a href="#">First level container</a>
+            <ul>
+                <li><a href="#">Second level entry</a></li>
+                <li><a href="#">Second level entry</a></li>
+            </ul>
+        </li>
+    </ul>

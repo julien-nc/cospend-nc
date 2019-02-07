5f47d43adc3240b588779e4b0298ff83df1dc949
Julien Veyssier <eneiluj@posteo.net>
2019-02-07 01:24:54 +0100

    write new translations

---
+++ l10n/en.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "payback",
+    {
+    "Payback" : "Payback"
+},
+"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: moneybuster\nX-Crowdin-Language: en-US");

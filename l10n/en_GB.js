3f861d1d7736d1035b7ce06ede86a737ec86e9c1
Julien Veyssier <eneiluj@posteo.net>
2019-02-12 02:27:13 +0100

    write new translations

---
+++ l10n/en_GB.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "cospend",
+    {
+    "Cospend" : "Cospend"
+},
+"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: moneybuster\nX-Crowdin-Language: en-GB");

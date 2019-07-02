6f079a67f36d691a75bf31a6c345775c3cf6fb6d
Julien Veyssier <eneiluj@posteo.net>
2019-07-02 23:00:08 +0200

    simpler translation update script [skip ci]

--- l10n/update.sh
+++ l10n/update.sh
@@ -3,9 +3,14 @@
 git checkout l10n_master
 git reset --hard HEAD~200
 git pull http l10n_master
-git rebase master
-git reset --soft master
-git commit -m "new translations from crowdin"
+rm -rf /tmp/translationfiles ; cp -r ../translationfiles /tmp
 git checkout master
-git merge l10n_master
+cp -r /tmp/translationfiles ../
+git commit -a -m "new translations from crowdin"
+
+#git rebase master
+#git reset --soft master
+#git commit -m "new translations from crowdin"
+#git checkout master
+#git merge l10n_master
 echo "MERGE DONE"

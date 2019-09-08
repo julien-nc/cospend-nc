c3142f00f6175015a1d5bcca510c07fd12c33685
Julien Veyssier <eneiluj@posteo.net>
2019-09-08 12:19:02 +0200

    remove useless functions

--- js/cospend.js
+++ js/cospend.js
@@ -68,58 +68,6 @@
         return 0.2126*rgb.r + 0.7152*rgb.g + 0.0722*rgb.b;
     }
 
-    function getLetterColor(letter1, letter2) {
-        var letter1Index = letter1.toLowerCase().charCodeAt(0);
-        var letter2Index = letter2.toLowerCase().charCodeAt(0);
-        var letterCoef = (letter1Index * letter2Index) % 100 / 100;
-        var h = letterCoef * 360;
-        var s = 45 + letterCoef * 10;
-        var l = 50 + letterCoef * 10;
-        return {h: Math.round(h), s: Math.round(s), l: Math.round(l)};
-    }
-
-    function hslToRgb(h, s, l) {
-        var r, g, b;
-
-        if(s == 0){
-            r = g = b = l; // achromatic
-        }else{
-            var hue2rgb = function hue2rgb(p, q, t){
-                if(t < 0) t += 1;
-                if(t > 1) t -= 1;
-                if(t < 1/6) return p + (q - p) * 6 * t;
-                if(t < 1/2) return q;
-                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-                return p;
-            };
-
-            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            var p = 2 * l - q;
-            r = hue2rgb(p, q, h + 1/3);
-            g = hue2rgb(p, q, h);
-            b = hue2rgb(p, q, h - 1/3);
-        }
-
-        //return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-        //return {r: Math.round(r * 255), g: Math.round(g * 255), b: Math.round(b * 255)};
-        var rgb = {r: Math.round(r * 255), g: Math.round(g * 255), b: Math.round(b * 255)};
-        var hexStringR = rgb.r.toString(16);
-        if (hexStringR.length % 2) {
-            hexStringR = '0' + hexStringR;
-        }
-        var hexStringG = rgb.g.toString(16);
-        if (hexStringG.length % 2) {
-            hexStringG = '0' + hexStringG;
-        }
-        var hexStringB = rgb.b.toString(16);
-        if (hexStringB.length % 2) {
-            hexStringB = '0' + hexStringB;
-        }
-        //console.log('r:'+hexStringR+' g:'+hexStringG+' b:'+hexStringB);
-        //console.log('rr:'+rgb.r+' gg:'+rgb.g+' bb:'+rgb.b);
-        return hexStringR+hexStringG+hexStringB;
-    }
-
     function Timer(callback, mydelay) {
         var timerId, start, remaining = mydelay;
 
@@ -1525,16 +1473,6 @@
         //var memberName = $('.projectitem[projectid="'+projectid+'"] .memberlist > li[memberid='+memberid+'] b.memberName').text();
         var memberName = cospend.members[projectid][memberid].name;
         return memberName;
-    }
-
-    function getMemberColor(memberName) {
-        var memberFirstLetter = memberName[0];
-        var memberSecondLetter = 'a';
-        if (memberName.length > 1) {
-            memberSecondLetter = memberName[1];
-        }
-        var c = getLetterColor(memberFirstLetter, memberSecondLetter);
-        return c;
     }
 
     function reload(msg) {
@@ -3067,7 +3005,6 @@
 
         // other bill fields : direct on edition
         $('body').on('change', '.input-bill-date, #billdetail select', function(e) {
-            console.log('ffffff');
             onBillEdited();
         });
 

03a7746e5f27e43f32e85a69e4832827e456cfe4
Julien Veyssier <eneiluj@posteo.net>
2019-12-31 17:30:33 +0100

    consider descriptions in translation update [skip ci]
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- l10n/update.sh
+++ l10n/update.sh
@@ -3,9 +3,11 @@
 git checkout l10n_master
 git reset --hard HEAD~200
 git pull origin l10n_master
-rm -rf /tmp/translationfiles ; cp -r ../translationfiles /tmp
+rm -rf /tmp/translationfiles ; cp -r ../translationfiles /tmp/
+rm -rf /tmp/descriptions ; cp -r ./descriptions /tmp/
 git checkout master
 cp -r /tmp/translationfiles ../
+cp -r /tmp/descriptions ./
 git commit -a -m "new translations from crowdin"
 
 #git rebase master

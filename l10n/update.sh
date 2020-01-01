53f9a507b1a11dbb27fc76f4fadb6471a46ff263
Julien Veyssier <eneiluj@posteo.net>
2020-01-01 12:51:52 +0100

    fix update script and reset full desc [skip ci]
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- l10n/update.sh
+++ l10n/update.sh
@@ -4,7 +4,7 @@ git checkout l10n_master
 git reset --hard HEAD~200
 git pull origin l10n_master
 rm -rf /tmp/translationfiles ; cp -r ../translationfiles /tmp/
-rm -rf /tmp/descriptions ; cp -r ./descriptions /tmp/
+rm -rf /tmp/descriptions ; mkdir /tmp/descriptions ; cp -r ./descriptions/[a-z][a-z]_[A-Z][A-Z] /tmp/descriptions/
 git checkout master
 cp -r /tmp/translationfiles ../
 cp -r /tmp/descriptions ./

48d4d3f2ec22e2f55cf7b4706e1f64267e3dce38
Julien Veyssier <eneiluj@posteo.net>
2020-01-06 11:49:48 +0100

    adapt tests
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- tests/php/controller/pageControllerTest.php
+++ tests/php/controller/pageControllerTest.php
@@ -209,7 +209,7 @@ class PageNUtilsControllerTest extends \PHPUnit\Framework\TestCase {
             $c->query('ServerContainer')->getConfig(),
             $c->getServer()->getAppManager(),
             $c->query('ServerContainer')->getAvatarManager(),
-            $c->query('ServerContainer')->getAppFolder(),
+            \OC\Server::getAppDataDir('cospend'),
             'test'
         );
     }

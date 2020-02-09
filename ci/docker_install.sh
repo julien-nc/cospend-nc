3c8dbe6aa1e5904ea61c1e70727847ffa7fd5807
Julien Veyssier <eneiluj@posteo.net>
2020-02-09 23:34:20 +0100

    use ubuntu19.10, nc18 and php7.3 in tests
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,7 +7,7 @@ set -xe
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -yqq
-apt-get install git sudo php-xdebug php7.2-sqlite3 php7.2-curl php7.2-xml php7.2-mbstring php7.2-json php7.2-zip php7.2-gd php7.2-intl unzip curl wget nodejs npm sed -yqq > /dev/null 2>&1
+apt-get install git sudo php-xdebug php7.3-sqlite3 php7.3-curl php7.3-xml php7.3-mbstring php7.3-json php7.3-zip php7.3-gd php7.3-intl unzip curl wget nodejs npm sed -yqq > /dev/null 2>&1
 npm install -g jshint > /dev/null 2>&1
 
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar

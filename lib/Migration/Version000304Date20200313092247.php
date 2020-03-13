a93b9b0c9857629d06fe3f5a3953470c13ea72fa
Julien Veyssier <eneiluj@posteo.net>
2020-03-13 15:38:05 +0100

    refs #48 add migration scripts
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

---
+++ lib/Migration/Version000304Date20200313092247.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Cospend\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\IDBConnection;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version000304Date20200313092247 extends SimpleMigrationStep {
+
+    /** @var IDBConnection */
+    private $connection;
+
+    /**
+     * @param IDBConnection $connection
+     */
+    public function __construct(IDBConnection $connection) {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+     * @param array $options
+     */
+    public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+    }
+
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+     * @param array $options
+     * @return null|ISchemaWrapper
+     */
+    public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+        /** @var ISchemaWrapper $schema */
+        $schema = $schemaClosure();
+
+        if ($schema->hasTable('cospend_bills')) {
+            $table = $schema->getTable('cospend_bills');
+            $table->addColumn('timestamp', 'bigint', [
+                'notnull' => true,
+                'length' => 10,
+                'default' => 0
+            ]);
+        }
+
+        return $schema;
+    }
+
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+     * @param array $options
+     */
+    public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+        $qb = $this->connection->getQueryBuilder();
+        $timestamps = [];
+        $qb->select('id', 'date')
+        ->from('cospend_bills', 'b');
+        $req = $qb->execute();
+        while ($row = $req->fetch()) {
+            $id = $row['id'];
+            $date = $row['date'];
+
+            $timestamp = strtotime($date.' 12:00:00');
+            $timestamps[$id] = $timestamp;
+        }
+        $req->closeCursor();
+        $qb = $qb->resetQueryParts();
+
+        foreach ($timestamps as $bid => $ts) {
+            $qb->update('cospend_bills')
+            ->set('timestamp', $qb->createNamedParameter($ts, IQueryBuilder::PARAM_INT))
+            ->where(
+                $qb->expr()->eq('id', $qb->createNamedParameter($bid, IQueryBuilder::PARAM_INT))
+            );
+            $qb->execute();
+            $qb = $qb->resetQueryParts();
+        }
+    }
+}

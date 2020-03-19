5a82fc6789afd33b64327dfcc315ff4eb81168f2
Julien Veyssier <eneiluj@posteo.net>
2020-03-19 16:11:07 +0100

    update l10n update [skip ci]
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- l10n/update.sh
+++ l10n/update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -rf /tmp/cospendjs
+mv ../js /tmp/cospendjs
+
 git checkout l10n_master
 git reset --hard HEAD~200
 git pull origin l10n_master
@@ -9,6 +12,8 @@ git checkout master
 cp -r /tmp/translationfiles ../
 cp -r /tmp/descriptions ./
 git commit -a -m "new translations from crowdin"
+
+mv /tmp/cospendjs ../js
 
 #git rebase master
 #git reset --soft master

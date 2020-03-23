83f7c4728706b70209a107698d488c11a4b375d5
Julien Veyssier <eneiluj@posteo.net>
2020-03-23 20:30:34 +0100

    refs #80 tired
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

---
+++ lib/Service/const.php
@@ -0,0 +1,21 @@
+<?php
+
+define('CAT_GROCERY', -1);
+define('CAT_BAR', -2);
+define('CAT_RENT', -3);
+define('CAT_BILL', -4);
+define('CAT_CULTURE', -5);
+define('CAT_HEALTH', -6);
+define('CAT_SHOPPING', -10);
+define('CAT_REIMBURSEMENT', -11);
+define('CAT_RESTAURANT', -12);
+define('CAT_ACCOMODATION', -13);
+define('CAT_TRANSPORT', -14);
+define('CAT_SPORT', -15);
+
+define('ACCESS_VIEWER', 1);
+define('ACCESS_PARTICIPANT', 2);
+define('ACCESS_MAINTENER', 3);
+define('ACCESS_ADMIN', 4);
+
+?>

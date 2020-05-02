b94bb610d0ec866fd73d0eda4315526783d270e1
Julien Veyssier <eneiluj@posteo.net>
2020-05-02 14:22:24 +0200

    stupid mistake in translation update [skip ci]
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- l10n/update.sh
+++ l10n/update.sh
@@ -3,7 +3,7 @@
 rm -rf /tmp/cospend
 git clone https://gitlab.com/eneiluj/cospend-nc /tmp/cospend -b l10n_master
 cp -r /tmp/cospend/l10n/descriptions/[a-z][a-z]_[A-Z][A-Z] ./descriptions/
-cp /tmp/cospend/l10n/*.js /tmp/cospend/l10n/*.json ./
-rm -rf /tmp/cospend
+cp -r /tmp/cospend/translationfiles/[a-z][a-z]_[A-Z][A-Z] ../translationfiles/
+#rm -rf /tmp/cospend
 
 echo "files copied"

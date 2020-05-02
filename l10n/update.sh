beba683dd275e599784f84d6922b71521562bb0f
Julien Veyssier <eneiluj@posteo.net>
2020-05-02 14:09:10 +0200

    simpler l10n update script [skip ci]
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- l10n/update.sh
+++ l10n/update.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-rm -rf /tmp/cospendjs
-mv ../js /tmp/cospendjs
-rm -rf /tmp/translationfiles ; cp -r ../translationfiles /tmp/
+rm -rf /tmp/cospend
+git clone https://gitlab.com/eneiluj/cospend-nc /tmp/cospend -b l10n_master
+cp -r /tmp/cospend/l10n/descriptions/[a-z][a-z]_[A-Z][A-Z] ./descriptions/
+cp /tmp/cospend/l10n/*.js /tmp/cospend/l10n/*.json ./
+rm -rf /tmp/cospend
 
-git checkout l10n_master
-git reset --hard HEAD~200
-git pull origin l10n_master
-rm -rf /tmp/descriptions ; mkdir /tmp/descriptions ; cp -r ./descriptions/[a-z][a-z]_[A-Z][A-Z] /tmp/descriptions/
-git checkout master
-cp -r /tmp/translationfiles ../
-cp -r /tmp/descriptions ./
-git commit -a -m "new translations from crowdin"
-
-mv /tmp/cospendjs ../js
-
-#git rebase master
-#git reset --soft master
-#git commit -m "new translations from crowdin"
-#git checkout master
-#git merge l10n_master
-echo "MERGE DONE"
+echo "files copied"

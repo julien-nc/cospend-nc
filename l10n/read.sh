c37cfbe46815f0b52c350b4e477ee52e052548bb
Julien Veyssier <eneiluj@posteo.net>
2020-06-03 21:02:06 +0200

    read new strings [skip ci]
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- l10n/read.sh
+++ l10n/read.sh
@@ -3,6 +3,8 @@
 cd ..
 rm -rf /tmp/cospendjs
 mv js /tmp/cospendjs
+mv node_modules /tmp/node_modules_cospend
 translationtool.phar create-pot-files
 mv /tmp/cospendjs js
+mv /tmp/node_modules_cospend node_modules
 cd -

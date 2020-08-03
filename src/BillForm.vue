12f1bedfee9155a91bcdc6669f40caf703296e59
Julien Veyssier <eneiluj@posteo.net>
2020-08-03 13:44:02 +0200

    refs #113 fix missing 'replaceAll' string method in chrom*
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/BillForm.vue
+++ src/BillForm.vue
@@ -325,7 +325,7 @@ export default {
                 return this.bill.amount;
             },
             set(value) {
-                const val = value.replaceAll(',', '.');
+                const val = value.replace(/,/g, '.');
                 // only change bill amount if we're not typing a formula
                 if (val === '') {
                     this.bill.amount = 0;
@@ -630,7 +630,7 @@ export default {
             }
         },
         onPersoAmountEnterPressed(e) {
-            const val = e.target.value.replaceAll(',', '.');
+            const val = e.target.value.replace(/,/g, '.');
             if (isNaN(val)) {
                 let calc = 'a';
                 try {
@@ -643,7 +643,7 @@ export default {
             }
         },
         onCustomAmountEnterPressed(e) {
-            const val = e.target.value.replaceAll(',', '.');
+            const val = e.target.value.replace(/,/g, '.');
             if (isNaN(val)) {
                 let calc = 'a';
                 try {

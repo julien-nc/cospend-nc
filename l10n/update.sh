e53ca7deea161600bf6180d48712cee06d7dba29
Julien Veyssier <eneiluj@posteo.net>
2020-10-18 18:49:47 +0200

    adapt translation update script
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- l10n/update.sh
+++ l10n/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf /tmp/cospend
-git clone https://gitlab.com/eneiluj/cospend-nc /tmp/cospend -b l10n_master
+git clone https://github.com/eneiluj/cospend-nc /tmp/cospend -b l10n_master
 cp -r /tmp/cospend/l10n/descriptions/[a-z][a-z]_[A-Z][A-Z] ./descriptions/
 cp -r /tmp/cospend/translationfiles/[a-z][a-z]_[A-Z][A-Z] ../translationfiles/
 rm -rf /tmp/cospend

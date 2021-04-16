f96cf6928d724ebd3bbc06825112b13de845ac0d
Julien Veyssier <eneiluj@posteo.net>
2021-04-16 13:19:38 +0200

    fix saving empty repeat freq [skip ci]
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/network.js
+++ src/network.js
@@ -275,7 +275,7 @@ export function saveBill(projectid, bill, successCB, doneCB) {
 		repeat: bill.repeat,
 		repeatallactive: bill.repeatallactive ? 1 : 0,
 		repeatuntil: bill.repeatuntil,
-		repeatfreq: bill.repeatfreq,
+		repeatfreq: bill.repeatfreq ? bill.repeatfreq : 1,
 		paymentmode: bill.paymentmode,
 		categoryid: bill.categoryid,
 	}

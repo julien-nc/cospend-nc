71f511aba581d876f7f419578c788dd49c2f8e4f
Julien Veyssier <eneiluj@posteo.net>
2021-06-28 14:43:27 +0200

    fix potentially incorrect category ids
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

---
+++ lib/Migration/Version010309Date20210628143307.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Cospend\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\IDBConnection;
+use OCP\IL10N;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version010309Date20210628143307 extends SimpleMigrationStep {
+
+	/** @var IDBConnection */
+	private $connection;
+	private $trans;
+
+	/**
+	 * @param IDBConnection $connection
+	 */
+	public function __construct(IDBConnection $connection, IL10N $l10n) {
+		$this->connection = $connection;
+		$this->trans = $l10n;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+		$qb = $this->connection->getQueryBuilder();
+
+		// fix potentially incorrect category ids < 0 and != -11
+		// => set them to -11 (reimbursement)
+		$qb->update('cospend_bills');
+		$qb->set('categoryid', $qb->createNamedParameter(-11, IQueryBuilder::PARAM_INT));
+		$qb->where(
+			$qb->expr()->lt('categoryid', $qb->createNamedParameter(0, IQueryBuilder::PARAM_INT))
+		);
+		$qb->andWhere(
+			$qb->expr()->neq('categoryid', $qb->createNamedParameter(-11, IQueryBuilder::PARAM_INT))
+		);
+		$qb->executeStatement();
+		$qb->resetQueryParts();
+	}
+}

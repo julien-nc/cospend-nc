07dfe5cf94405bc6864766f941897ce07be669b2
Julien Veyssier <eneiluj@posteo.net>
2021-07-28 15:36:41 +0200

    rich content for sidebar activity entries
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/components/ActivityEntry.vue
+++ src/components/ActivityEntry.vue
@@ -2,7 +2,14 @@
 	<div class="activity-entry">
 		<span class="icon-activity-entry"
 			:style="{ 'background-image': 'url(\'' + iconUrl + '\')' }" />
-		<span class="subject">
+		<span
+			v-if="subjectRich"
+			class="subject">
+			<RichText
+				:text="subjectRich"
+				:arguments="subjectParameters" />
+		</span>
+		<span v-else>
 			{{ activity.subject }}
 		</span>
 		<span class="time">
@@ -15,6 +22,10 @@
 
 import { generateUrl } from '@nextcloud/router'
 import moment from '@nextcloud/moment'
+import { getCurrentUser } from '@nextcloud/auth'
+import RichText from '@juliushaertl/vue-richtext'
+
+import UserBubble from '@nextcloud/vue/dist/Components/UserBubble'
 
 const isDarkTheme = OCA.Accessibility.theme === 'dark'
 const icons = {
@@ -29,6 +40,9 @@ export default {
 	name: 'ActivityEntry',
 
 	components: {
+		RichText,
+		// eslint-disable-next-line
+		UserBubble,
 	},
 
 	props: {
@@ -49,12 +63,66 @@ export default {
 		relativeTime() {
 			return moment(this.activity.datetime).fromNow()
 		},
+		subjectRich() {
+			const actionUserIsMe = this.userIsMe(this.activity.subject_rich[1]?.user?.id)
+			if (this.activity.link === 'bill_update') {
+				return actionUserIsMe
+					? t('cospend', '{user} have updated the bill {billName}', { billName: this.activity.subject_rich[1]?.bill?.name })
+					: t('cospend', '{user} has updated the bill {billName}', { billName: this.activity.subject_rich[1]?.bill?.name })
+			} else if (this.activity.link === 'bill_create') {
+				return actionUserIsMe
+					? t('cospend', '{user} have created a new bill {billName}', { billName: this.activity.subject_rich[1]?.bill?.name })
+					: t('cospend', '{user} has created a new bill {billName}', { billName: this.activity.subject_rich[1]?.bill?.name })
+			} else if (this.activity.link === 'bill_delete') {
+				return actionUserIsMe
+					? t('cospend', '{user} have deleted the bill {billName}', { billName: this.activity.subject_rich[1]?.bill?.name })
+					: t('cospend', '{user} has deleted the bill {billName}', { billName: this.activity.subject_rich[1]?.bill?.name })
+			} else if (this.activity.link === 'project_share') {
+				return actionUserIsMe
+					? t('cospend', '{user} have shared the project with {who}')
+					: t('cospend', '{user} has shared the project with {who}')
+			} else if (this.activity.link === 'project_unshare') {
+				return actionUserIsMe
+					? t('cospend', '{user} have removed {who} from the project')
+					: t('cospend', '{user} has removed {who} from the project')
+			}
+			return null
+		},
+		subjectParameters() {
+			const params = {}
+			if (['bill_update', 'bill_create', 'bill_delete', 'project_share', 'project_unshare'].includes(this.activity.link)) {
+				params.user = {
+					component: UserBubble,
+					props: {
+						user: this.activity.subject_rich[1]?.user?.id || undefined,
+						displayName: this.userIsMe(this.activity.subject_rich[1]?.user?.id)
+							? t('cospend', 'You')
+							: this.activity.subject_rich[1]?.user?.name,
+					},
+				}
+			}
+			if (['project_share', 'project_unshare'].includes(this.activity.link)) {
+				params.who = {
+					component: UserBubble,
+					props: {
+						user: this.activity.subject_rich[1]?.who?.id || undefined,
+						displayName: this.userIsMe(this.activity.subject_rich[1]?.who?.id)
+							? t('cospend', 'you')
+							: this.activity.subject_rich[1]?.who?.name,
+					},
+				}
+			}
+			return params
+		},
 	},
 
 	mounted() {
 	},
 
 	methods: {
+		userIsMe(userId) {
+			return getCurrentUser().uid === userId
+		},
 	},
 }
 </script>
@@ -63,7 +131,7 @@ export default {
 .activity-entry {
 	display: flex;
 	align-items: center;
-	margin: 5px 0 5px 0;
+	margin: 8px 0 8px 0;
 	&:hover {
 		background-color: var(--color-background-hover);
 	}

f2876f9e51ec676f829d9cc2f2827b044823718e
Julien Veyssier <eneiluj@posteo.net>
2021-07-29 17:54:18 +0200

    bump js libs
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- webpack.js
+++ webpack.js
@@ -4,6 +4,7 @@ const webpackConfig = require('@nextcloud/webpack-vue-config')
 const buildMode = process.env.NODE_ENV
 const isDev = buildMode === 'development'
 webpackConfig.devtool = isDev ? 'cheap-source-map' : 'source-map'
+// webpackConfig.bail = false
 
 webpackConfig.stats = {
     colors: true,

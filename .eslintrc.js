f4c79266b3edabe0b10879d2ff5821711ca33c41
Julien Veyssier <eneiluj@posteo.net>
2022-04-25 14:37:15 +0200

    bump js libs
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
 	rules: {
 		'jsdoc/require-jsdoc': 'off',
 		'jsdoc/tag-lines': 'off',
-		'vue/first-attribute-linebreak': 'off'
+		'vue/first-attribute-linebreak': 'off',
+		'import/extensions': 'off'
 	}
 }

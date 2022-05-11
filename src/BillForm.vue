392d8a3cb5684146a7ea6100f0d02f9d3f376f8f
Julien Veyssier <eneiluj@posteo.net>
2022-05-11 15:52:32 +0200

    nothing
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/BillForm.vue
+++ src/BillForm.vue
@@ -1056,6 +1056,7 @@ export default {
 				: moment(date).locale(this.locale).format('LL')
 		},
 		parse(value) {
+			// in case we detect another format
 			if (value.match(/^\d\d.\d\d.\d\d\d\d$/)) {
 				return moment(value, 'DD.MM.YYYY').toDate()
 			} else if (value.match(/^\d\d\/\d\d\/\d\d\d\d$/)) {

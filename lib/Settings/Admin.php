c49e9da5134049de9ab408042d4d3e2a7946d1eb
Julien Veyssier <eneiluj@posteo.net>
2022-06-19 16:11:14 +0200

    bring tabs back in lib/Settings/Admin.php
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- lib/Settings/Admin.php
+++ lib/Settings/Admin.php
@@ -6,48 +6,47 @@ use OCP\AppFramework\Http\TemplateResponse;
 use OCP\IConfig;
 use OCP\Settings\ISettings;
 
-class Admin implements ISettings
-{
-    /**
-     * @var IConfig
-     */
-    private $config;
+class Admin implements ISettings {
+	/**
+	 * @var IConfig
+	 */
+	private $config;
 
-    public function __construct(IConfig $config)
-    {
-        $this->config = $config;
-    }
+	public function __construct(IConfig $config)
+	{
+		$this->config = $config;
+	}
 
-    /**
-     * @return TemplateResponse
-     */
-    public function getForm(): TemplateResponse
-    {
-        $allow = $this->config->getAppValue('cospend', 'allowAnonymousCreation');
+	/**
+	 * @return TemplateResponse
+	 */
+	public function getForm(): TemplateResponse
+	{
+		$allow = $this->config->getAppValue('cospend', 'allowAnonymousCreation');
 
-        $parameters = [
-            'allowAnonymousCreation' => $allow
-        ];
-        return new TemplateResponse('cospend', 'admin', $parameters, '');
-    }
+		$parameters = [
+			'allowAnonymousCreation' => $allow
+		];
+		return new TemplateResponse('cospend', 'admin', $parameters, '');
+	}
 
-    /**
-     * @return string the section ID, e.g. 'sharing'
-     */
-    public function getSection(): string
-    {
-        return 'additional';
-    }
+	/**
+	 * @return string the section ID, e.g. 'sharing'
+	 */
+	public function getSection(): string
+	{
+		return 'additional';
+	}
 
-    /**
-     * @return int whether the form should be rather on the top or bottom of
-     * the admin section. The forms are arranged in ascending order of the
-     * priority values. It is required to return a value between 0 and 100.
-     *
-     * E.g.: 70
-     */
-    public function getPriority(): int
-    {
-        return 5;
-    }
+	/**
+	 * @return int whether the form should be rather on the top or bottom of
+	 * the admin section. The forms are arranged in ascending order of the
+	 * priority values. It is required to return a value between 0 and 100.
+	 *
+	 * E.g.: 70
+	 */
+	public function getPriority(): int
+	{
+		return 5;
+	}
 }

73e7c2ecbeeb40f8e2943f952cc4aeeae510b50b
Julien Veyssier <julien-nc@posteo.net>
2026-02-19 00:32:53 +0100

    adjust l10n read script, read them
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- l10n/read.sh
+++ l10n/read.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 cd ..
-rm -rf /tmp/cospendjs
-mv js /tmp/cospendjs
-mv node_modules /tmp/node_modules_cospend
 translationtool.phar create-pot-files
-mv /tmp/cospendjs js
-mv /tmp/node_modules_cospend node_modules
 cd -
